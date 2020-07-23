7664eed95976c9eeea84acc35fd113c545b11974
mohamedhawas123 <mohamedhawas123@gmail.com>
2020-07-23 11:32:11 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,9 +108,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # Password validation
