2149a8852c31808844950158b3c8262455cffb78
mohamedhawas123 <mohamedhawas123@gmail.com>
2020-07-23 11:39:22 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASE_URL = os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
